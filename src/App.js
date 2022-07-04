2143346cb9dc49d5a7113644bd4424baa9fa5d3e
rajeshmn47 <rajeshmn47@gmail.com>
2022-07-04 06:21:45 +0530

    task completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect,useState } from 'react';
+import axios from 'axios'
+import SearchIcon from '@mui/icons-material/Search';
+import MenuIcon from '@mui/icons-material/Menu';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 
-function App() {
+
+function App() {  
+  const[users,setUsers]=useState()
+  useEffect(()=>{
+    async function getusers(){
+      const data= await axios.get('https://randomuser.me/api?results=500')
+      const use=data.data.results.sort((a,b)=>{if(a.name.first>b.name.first){
+        return 1;
+      }
+      else {
+        return -1;
+      }})
+      setUsers(use)
+          }
+          getusers()
+  },[])
+  var z='A'
+  const firstletter=(str)=>{
+if((z===str)){
+  console.log(str,z,str===z)
+  z=str
+  console.log(str,z,str===z)
+  return z
+}
+  else{
+  return null;
+  }
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+   
+   <div className='topbar'>
+    <div className='topbar'>
+   <MenuIcon/>
+   <h5 style={{display:'flex',alignItems:'center',marginLeft:'1vw'}}>All <ArrowDropDownIcon/></h5>
+   </div>
+   <div className='topbar'>
+  <SearchIcon style={{marginRight:'1vw'}}/>
+  <MoreVertIcon/>
+  </div>
+  </div>
+   <div className='contacts'>
+   {users&&users.map((u)=><>
+   <div className='contact'>
+    <h5 style={{color:'red'}}>{firstletter(u.name.first[0])&&firstletter(u.name.first[0])}</h5>
+   <h5>{u.name.first} {u.name.last}</h5>
+   <img src={u.picture.thumbnail} alt='' width='40' style={{borderRadius:'50%'}}/>
+   </div>
+   </>)}
+   </div>
     </div>
   );
 }
