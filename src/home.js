cde9c22ddf93062b309c49955d45e6345f750223
rajeshmn47 <rajeshmn47@gmail.com>
2022-07-04 13:44:44 +0530

    task completed

--- src/home.js
+++ src/home.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { useEffect,useState } from 'react';
+import { useEffect,useState,useRef } from 'react';
 import axios from 'axios'
 import SearchIcon from '@mui/icons-material/Search';
 import MenuIcon from '@mui/icons-material/Menu';
@@ -12,38 +12,50 @@ import Skelton from './skeleton'
 import {useNavigate} from 'react-router-dom'
 
 
+
 function Home() {  
     const navigate=useNavigate()
-  const[users,setUsers]=useState()
+    const listInnerRef = useRef();
+    const [Loading,setLoading]=useState(false)
+  const[users,setUsers]=useState([])
+  async function getusers(){
+    setLoading(true)
+    const data= await axios.get('https://randomuser.me/api?results=25')
+    setUsers([...users,...data.data.results])
+    setLoading(false)
+        }
   useEffect(()=>{
-    async function getusers(){
-      const data= await axios.get('https://randomuser.me/api?results=500')
-      const use=data.data.results.sort((a,b)=>{if(a.name.first>b.name.first){
-        return 1;
-      }
-      else {
-        return -1;
-      }})
-      setUsers(use)
-          }
           getusers()
   },[])
   var z='4'
   const firstletter=(str)=>{
 if(!(z===str)){
-  console.log(str,z,str===z)
   z=str
-  console.log(str,z,str===z)
+ 
   return z;
 }
   else{
-    console.log('rajesh')
+    
   return null;
   }
 }
+
+const onscroll = () => {
+    console.log('rajesh')
+    if (listInnerRef.current) {
+      const { scrollTop, scrollHeight, clientHeight } = listInnerRef.current;
+      console.log(scrollTop, scrollHeight, clientHeight)
+      if (scrollTop + clientHeight === scrollHeight) {
+        // TO SOMETHING HERE
+        console.log('Reached bottom')
+        setLoading(true)
+        getusers()
+        setLoading(false)
+      }
+    }
+  };
   return (
-    <div>
-   
+    <div  onScroll={onscroll} ref={listInnerRef}>
    <div className='topbar'>
     <div className='topbar'>
    <MenuIcon/>
@@ -56,14 +68,15 @@ if(!(z===str)){
   </div>
   </div>
    <div className='contacts'>
-   {users?users.map((u)=><>
-   <div className='contact'>
-    <h1 style={{color:'red'}}>{firstletter(u.name.first[0])}</h1>
-   <h5>{u.name.first} {u.name.last}</h5>
-   <img src={u.picture.thumbnail} alt='' width='40' style={{borderRadius:'50%'}}/>
+   {users&&users.map((u)=><>
+   <div className='contact' key={u.name.first}>
+    <h1 style={{color:'red'}} key={u.name.last}>{firstletter(u.name.first[0])}</h1>
+   <h5 key={u.name.first}>{u.name.first} {u.name.last+u.name.last}</h5>
+   <img key={u.name.first+u.picture.thumbnail} src={u.picture.thumbnail} alt='' width='40' style={{borderRadius:'50%'}}/>
    </div>
-   </>):<Skelton />
-}
+   </>)}
+   {Loading&&<Skelton />}
+
    </div>
     </div>
   );
